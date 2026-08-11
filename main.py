81a01360b718b02ceeaf0da7c8dd19db326de26a
vince <vinceiansuralta3@gmail.com>
2026-08-11 18:49:57 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from player import Player
+from enemy import Enemy
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    p = Player()
+    print("Game Started!")
